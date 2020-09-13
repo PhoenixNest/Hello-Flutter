3353dbb208a77d2923a36b7b1198539b6f05f3ae
PhoenixNest <XenOmeGA@163.com>
2020-09-13 17:58:16 +0800

    Learning Flutter Day01 - Counter

--- day01_helloworld/lib/main.dart
+++ day01_helloworld/lib/main.dart
@@ -11,53 +11,57 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text("Apple Dev Center"),
           ),
-          body: Content(),
+          body: ContentWidget(),
         ));
   }
 }
 
-class Content extends StatelessWidget {
+class ContentWidget extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: ListView(
-        children: <Widget>[
-          Item("Apple 1", "Dev Center",
-              "https://n.sinaimg.cn/spider2020913/372/w750h422/20200913/2d1a-iyywcta1136728.jpg"),
-          Item("Apple 2", "Dev Center",
-              "https://n.sinaimg.cn/spider2020913/449/w750h499/20200913/cc28-iyywcta1136729.jpg"),
-          Item("Apple 3", "Dev Center",
-              "https://n.sinaimg.cn/spider2020913/372/w750h422/20200913/e13f-iyywcta1136773.jpg"),
-        ],
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return ContentWidgetState();
   }
 }
 
-class Item extends StatelessWidget {
-  final String title;
-  final String type;
-  final String imageUrl;
-
-  Item(this.title, this.type, this.imageUrl);
+class ContentWidgetState extends State<ContentWidget> {
+  int currentNum = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(2),
-      decoration: BoxDecoration(border: Border.all(width: 2)),
+    return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text("+1"),
+                onPressed: () {
+                  print("Press");
+                  // 监听状态改变
+                  setState(() {
+                    currentNum++;
+                  });
+                },
+              ),
+              SizedBox(width: 10),
+              RaisedButton(
+                child: Text("-1"),
+                onPressed: () {
+                  print("Press");
+                  // 监听状态改变
+                  setState(() {
+                    currentNum--;
+                  });
+                },
+              ),
+            ],
+          ),
           Text(
-            title,
+            "当前计数: $currentNum",
             style: TextStyle(fontSize: 24),
           ),
-          Text(type, style: TextStyle(fontSize: 18)),
-          SizedBox(
-            height: 10,
-          ),
-          Image.network(imageUrl),
         ],
       ),
     );
