398e7063a13338a1624a8fcdaaf7016707fcfc25
PhoenixNest <XenOmeGA@163.com>
2020-09-14 20:49:32 +0800

    Learning Flutter Day02 - Basic Widget - Text

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -8,83 +8,61 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: "Hello World",
         home: Scaffold(
-          appBar: AppBar(
-            title: Text("Dev Center"),
-          ),
-          body: ContentWidget(),
-        ));
+            appBar: AppBar(
+              title: Text("Dev Center"),
+            ),
+            body: ContentWidget()));
   }
 }
 
-/// 生命周期函数调用顺序：
-/// (1). Widget构造函数 ->
-/// (2). Widget - createState ->
-/// (3). WidgetState - 构造函数 ->
-/// (4). WidgetState - initState ->
-/// (5). WidgetState - didChangeDependencies ->
-/// (6). WidgetState - build ->
-/// (7). (如果对页面元素进行改变则会重新调用build方法) WidgetState - build ->
-
-class ContentWidget extends StatefulWidget {
-  ContentWidget() {
-    print("ContentWidget构造函数");
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    print("ContentWidget - createState");
-    return ContentWidgetState();
-  }
-}
-
-class ContentWidgetState extends State<ContentWidget> {
-  int currentNum = 0;
-
-  ContentWidgetState() {
-    print("ContentWidgetState构造函数");
-  }
-
-  @override
-  void initState() {
-    // 父类种可能调用的方法
-    super.initState();
-
-    print("ContentWidgetState - initState");
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print("ContentWidgetState - didChangeDependencies");
-  }
-
+class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("ContentWidgetState - build");
-
-    return Center(
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text("+1"),
-            onPressed: () {
-              setState(() {
-                currentNum++;
-              });
-            },
-          ),
-          Text(
-            "Count: $currentNum",
-            style: TextStyle(fontSize: 24),
-          ),
-        ],
-      ),
+    return Text.rich(
+      TextSpan(children: [
+        TextSpan(
+            text: "Text1",
+            style: TextStyle(
+                fontSize: 56,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent)),
+        TextSpan(
+            text: "Text2Text2",
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.green)),
+        TextSpan(
+            text: "Text3Text3Text3",
+            style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue)),
+        TextSpan(
+            text: "Text4Text4Text4Text4",
+            style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.orange))
+      ]),
+      textAlign: TextAlign.center,
     );
   }
-
-  @override
-  void didUpdateWidget(ContentWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("ContentWidgetState - didUpdateWidget");
-  }
 }
+
+// class TextDemo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       "Test Text \n\n Test Text, Test Text, Test Text, Test Text",
+//       style: TextStyle(fontSize: 24, color: Colors.orange),
+//       textAlign: TextAlign.center,
+//       // 最大显示行数: maxLines
+//       // maxLines: 2,
+//       // 超出部分显示方式: overflow
+//       // overflow: TextOverflow.ellipsis,
+//       // 缩放倍数: textScaleFactor
+//       // textScaleFactor: 2,
+//     );
+//   }
+// }
