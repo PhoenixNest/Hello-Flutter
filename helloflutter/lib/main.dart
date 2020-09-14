42119bfd662e63c3bdbca32d38ba9fd3999f65b8
PhoenixNest <XenOmeGA@163.com>
2020-09-14 21:18:45 +0800

    Learning Flutter Day02 - Basic Widget - Button

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -18,38 +18,111 @@ class MyApp extends StatelessWidget {
 class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(children: [
-        TextSpan(
-            text: "Text1",
-            style: TextStyle(
-                fontSize: 56,
-                fontWeight: FontWeight.bold,
-                color: Colors.redAccent)),
-        TextSpan(
-            text: "Text2Text2",
-            style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.green)),
-        TextSpan(
-            text: "Text3Text3Text3",
-            style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue)),
-        TextSpan(
-            text: "Text4Text4Text4Text4",
-            style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Colors.orange))
-      ]),
-      textAlign: TextAlign.center,
+    return CustomButtonDemo();
+  }
+}
+
+/// Widget - CustomButtonDemo
+class CustomButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        /// 圆角: RoundedRectangleBorder
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        color: Colors.blueAccent,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              Icons.adb,
+              color: Colors.white,
+            ),
+            SizedBox(width: 12),
+            Text(
+              "CustomRaisedButton",
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+        onPressed: () {
+          print("CustomRaisedButton");
+        },
+      ),
     );
   }
 }
 
+/// Widget - Button
+// class ButtonDemo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         RaisedButton(
+//           child: Text("RaisedButton"),
+//           onPressed: () {
+//             print("RaisedButton");
+//           },
+//         ),
+//         FlatButton(
+//           onPressed: () {
+//             print("FlatButton");
+//           },
+//           child: Text("FlatButton"),
+//         ),
+//         FloatingActionButton(
+//           child: Text("FloatingActionButton"),
+//           onPressed: () {
+//             print("FloatingActionButton");
+//           },
+//         ),
+//         OutlineButton(
+//           child: Text("OutlineButton"),
+//           onPressed: () {
+//             print("OutlineButton");
+//           },
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+/// Widget - Text.RichText
+// class TextRichDemo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text.rich(
+//       TextSpan(children: [
+//         TextSpan(
+//             text: "Text1",
+//             style: TextStyle(
+//                 fontSize: 56,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.redAccent)),
+//         TextSpan(
+//             text: "Text2Text2",
+//             style: TextStyle(
+//                 fontSize: 28,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.green)),
+//         TextSpan(
+//             text: "Text3Text3Text3",
+//             style: TextStyle(
+//                 fontSize: 14, fontWeight: FontWeight.bold, color: Colors.blue)),
+//         TextSpan(
+//             text: "Text4Text4Text4Text4",
+//             style: TextStyle(
+//                 fontSize: 14,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.orange))
+//       ]),
+//       textAlign: TextAlign.center,
+//     );
+//   }
+// }
+
+/// Widget - Text
 // class TextDemo extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
