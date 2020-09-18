b3a2926e52d94d6dc01a8a0a10c7a56717f251f2
PhoenixNest <XenOmeGA@163.com>
2020-09-18 17:00:34 +0800

    Learning Flutter Day03 - Basic Widget - Form validator

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -47,14 +47,25 @@ class myFormState extends State<FormDemo> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
+              autovalidate: true,
               decoration: InputDecoration(
                   icon: Icon(Icons.people), labelText: "Input userName"),
               onSaved: (v) {
                 this.userName = v;
                 print("userName onSave");
               },
+              //验证输入框内容是否合法
+              validator: (v) {
+                if (v.length == null || v.length == 0) {
+                  //未监测到输入，并提示信息
+                  return "Please input userName";
+                }
+                //验证通过，不提示信息
+                return null;
+              },
             ),
             TextFormField(
+              autovalidate: true,
               // 密文输入
               obscureText: true,
               decoration: InputDecoration(
@@ -62,6 +73,15 @@ class myFormState extends State<FormDemo> {
               onSaved: (v) {
                 this.passWord = v;
                 print("passWord onSave");
+              },
+              //验证输入框内容是否合法
+              validator: (v) {
+                if (v.length == null || v.length == 0) {
+                  //未监测到输入，并提示信息
+                  return "Please input passWord";
+                }
+                //验证通过，不提示信息
+                return null;
               },
             ),
             SizedBox(height: 16),
@@ -74,10 +94,12 @@ class myFormState extends State<FormDemo> {
                   print("onPress");
                   //通过下述方法统一接受输入框的值
                   globalKey.currentState.save();
+                  //通过下述方法验证输入框输入内容是否合法，当TextFormField开启了autovalidate时，可以进行自动验证输入，此时可省去下述方法
+                  // globalKey.currentState.validate();
                   print("userName: $userName --- password: $passWord");
                 },
                 child: Text(
-                  "Register",
+                  "Login",
                   style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
               ),
